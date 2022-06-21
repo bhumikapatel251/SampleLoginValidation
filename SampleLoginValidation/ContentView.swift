8693aff5b936e306cc07cfa3399eb97f07d1b761
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-06-21 16:34:22 +0530

    Initial Commit

---
+++ SampleLoginValidation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleLoginValidation
+//
+//  Created by Bhumika Patel on 21/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleLoginValidation/SampleLoginValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLoginValidationApp.swift
+//  SampleLoginValidation
+//
+//  Created by Bhumika Patel on 21/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleLoginValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
